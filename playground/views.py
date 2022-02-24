8c70d6e17eafee0a904bc8f19fa5327f4a32cb80
unknown <henokmekuanint79@gmail.com>
2022-02-24 16:23:12 +0300

    somemodificationaremade

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+def signup(request):
+    return render(request,'signup.html')
+def signin(request):
+    return render(request,'signin.html')
+def signout(request):
+    return render(request,'signout.html')
