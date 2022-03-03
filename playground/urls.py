40928b315a2cfd3a09be2409810cf2bdd368c6d5
unknown <henokmekuanint79@gmail.com>
2022-03-03 15:10:48 +0300

    finally

--- playground/urls.py
+++ playground/urls.py
@@ -5,6 +5,9 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 urlpatterns = [
     path('',views.home,name='home'),
-    path('accounts/login',auth_views.LoginView.as_view(),name='login'),
-    path('',TemplateView.as_view(template_name='HOME.html'),name="home")    
+    path('signup',views.user_signup,name='signup'),
+    path('login',views.user_login,name='login'),
+    path('logout',views.user_logout,name='logout'),
+    path('profile/<int:user_id',views.user_profile,name='userprofile'),
+
 ]
