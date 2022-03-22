a3174fc1a2b9aa94345f0fbd9ace026afe0067b1
unknown <henokmekuanint79@gmail.com>
2022-03-22 10:08:42 +0300

    trytopull

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,8 @@
-import email
 from operator import itemgetter
-from telnetlib import ENCRYPT
-import MySQLdb
-from django.http import HttpResponse
-from tkinter.messagebox import Message
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
-from mysqlx import Auth
-from.models import UserProfile
-from django.core.files.storage import FileSystemStorage
 import mysql.connector
 from django.contrib.auth.models import User
 def home(request):
@@ -22,21 +13,25 @@ def user_signup(request):
         username=request.POST['username']
         firstname=request.POST['firstname']
         lastname=request.POST['lastname']
-        password=request.POST['password']        
+        password=request.POST['password']
+        conpassword=request.POST['password2']
+        if password!=conpassword:
+            messages.info(request,"password not confiremed please enter the correct password to confirm")
+            return redirect('signup')        
         if User.objects.filter(username=username):
-            messages.error(request, "Username already exist! Please try some other username.")
+            messages.info(request, "Username already exist! Please try some other username.")
             return redirect('signup')
         
         if User.objects.filter(email=user_email).exists():
-            messages.error(request, "Email Already Registered!!")
+            messages.info(request, "Email Already Registered!!")
             return redirect('signup')
         
         if len(username)>20:
-            messages.error(request, "Username must be under 20 charcters!!")
+            messages.info(request, "Username must be under 20 charcters!!")
             return redirect('signup')
         
         if not username.isalnum():
-            messages.error(request, "Username must be Alpha-Numeric!!")
+            messages.info(request, "Username must be Alpha-Numeric!!")
             return redirect('signup')
         user_obj = User.objects.create_user(username, user_email)
         user_obj.first_name = firstname
@@ -51,26 +46,12 @@ def user_signup(request):
         return render(request,'signup.html')   
 
 def user_login(request):
-    con1=mysql.connector.connect(host="localhost",user="root",password="0962081628@hm",database="hena")
-    cursor=con1.cursor()
-    sqlcommand1="select username from auth_user"
-
-
-    cursor.execute(sqlcommand1)
-    u=[]
-    
-    print(cursor)
-    for i in cursor:
-        u.append(i)
-    res = list(map(itemgetter(0),u))
     if request.method=='POST':
         username=request.POST.get('username')
         userpass=request.POST.get('password')
         check_if_user_exists = User.objects.filter(username=username).exists()
 
-        if username in res and check_if_user_exists :
-                User.is_authenticated=True
-
+        if check_if_user_exists :
                 con3=mysql.connector.connect(host="localhost",user="root",password="0962081628@hm",database="hena")
                 cursor3=con3.cursor()
                 sqlcommand3="select password from auth_user where username=%s"
@@ -79,11 +60,12 @@ def user_login(request):
                 for i in cursor3:
                     n.append(i)
                 res3=list(map(itemgetter(0),n))
-                print(res3)
-
+                
                 if res3[0]==userpass:
                     messages.info(request, "Your have logged in successfully")
                     messages.info(request,"hi  "+username)
+                    User.is_authenticated=True
+                    print(User.is_authenticated)
                     return render(request,'home.html',)
                 else:
                     messages.info(request,"you have enterd incorrect password")
